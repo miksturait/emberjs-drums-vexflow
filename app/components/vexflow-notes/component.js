f783a82af537b77b79bec9818b0d5635307c1b06
Michał Czyż <m.czyz@selleo.com>
2016-11-30 10:36:45 +0100

    render notes

--- app/components/vexflow-notes/component.js
+++ app/components/vexflow-notes/component.js
@@ -19,7 +19,24 @@ export default Ember.Component.extend({
     this._super(...arguments);
 
     let stave = new this.vf.Stave(10, 40, 400);
-    stave.addClef("treble").addTimeSignature("4/4");
+    stave.addClef("bass").addTimeSignature("4/4");
     stave.setContext(this.vfContext).draw();
+
+    let dataNotes = [
+      {clef: "bass", keys: ["c/2"], duration: "8"},
+      {clef: "bass", keys: ["c/2"], duration: "8"},
+      {clef: "bass", keys: ["d/2"], duration: "q"},
+      {clef: "bass", keys: ["b/2"], duration: "q"},
+      {clef: "bass", keys: ["c/2", "e/2", "g/2"], duration: "q"}
+    ];
+
+    let notes = dataNotes.map(_note => new this.vf.StaveNote(_note));
+    let voice = new this.vf.Voice({num_beats: 4, beat_value: 4});
+    let formatter = new this.vf.Formatter();
+
+    voice.addTickables(notes);
+    formatter.joinVoices([voice]).format([voice], 400);
+
+    voice.draw(this.vfContext, stave);
   }
 });
